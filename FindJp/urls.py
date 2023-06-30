96f43c5de48eccc537800d9b506e5780d4b1f2d8
juanpabloct <juanptorres2003@gmail.com>
2023-06-30 18:21:00 -0500

    Change of routes

--- FindJp/urls.py
+++ FindJp/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("satelite/", include("satelite.urls")),
+    path("", include("satelite.urls")),
 ]
--- satelite/urls.py
+++ satelite/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'satelite'
 
 urlpatterns = [
-    path('', views.satelite_views, name='satelite_views'),
+    path('localizacion/', views.satelite_views, name='satelite_views'),
     path('localizacion_por_partes/<str:nombre_antena>/', views.localizacion_por_partes_views, name='localizacion_por_partes_views'),
     # Otras URLs de la aplicación "satelite"...
 ]
